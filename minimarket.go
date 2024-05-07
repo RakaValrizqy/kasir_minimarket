0041f8d39f8e1a555b14c263de18f1b75fbd8db7
RakaValrizqy <myemail>
2024-05-07 20:00:23 +0700

    struct produk, transaksi

--- minimarket.go
+++ minimarket.go
@@ -1,6 +1,22 @@
 package main
 import "fmt"
 
+const nMax int = 1000
+
+type produk struct {
+	nama string
+	harga int
+}
+
+type transaksi struct {
+	prod produk
+	banyak int
+	subtotal int
+}
+
+type tabProd [nMax]produk
+type tabTrans [nMax]transaksi
+
 func main(){
-	
+
 }
